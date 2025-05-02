46a931abf3381ecf70caff93d7e0f2016b0a4661
izZziCOS <50332439+izZziCOS@users.noreply.github.com>
2025-05-02 15:15:57 +0300

    comments

--- api/internal/mocks/user_repository.go
+++ api/internal/mocks/user_repository.go
@@ -8,25 +8,30 @@ import (
 	"github.com/stretchr/testify/mock"
 )
 
+// UserRepository is a mock implementation of repository.UserRepository interface
 type UserRepository struct {
 	mock.Mock
 }
 
+// Create mocks the user creation
 func (m *UserRepository) Create(ctx context.Context, user *models.User) error {
 	args := m.Called(ctx, user)
 	return args.Error(0)
 }
 
+// Update mocks updating the user
 func (m *UserRepository) Update(ctx context.Context, user *models.User) error {
 	args := m.Called(ctx, user)
 	return args.Error(0)
 }
 
+// Delete mocks deletion of user
 func (m *UserRepository) Delete(ctx context.Context, id string) error {
 	args := m.Called(ctx, id)
 	return args.Error(0)
 }
 
+// FindAll mock finding matching users by filters with pagination
 func (m *UserRepository) FindAll(ctx context.Context, filter repository.UserFilter, pagination repository.Pagination) ([]models.User, error) {
 	args := m.Called(ctx, filter, pagination)
 
@@ -34,6 +39,7 @@ func (m *UserRepository) FindAll(ctx context.Context, filter repository.UserFilt
 	return users, args.Error(1)
 }
 
+// FindByID finds specific user by specified ID
 func (m *UserRepository) FindByID(ctx context.Context, id string) (*models.User, error) {
 	args := m.Called(ctx, id)
 
--- api/internal/mocks/user_service.go
+++ api/internal/mocks/user_service.go
@@ -1,3 +1,4 @@
+// Provides mocks for User Service
 package mocks
 
 import (
@@ -8,10 +9,12 @@ import (
 	"github.com/stretchr/testify/mock"
 )
 
+// UserService is a mock implementation of contracts.UserService interface
 type UserService struct {
 	mock.Mock
 }
 
+// CreateUser mocks user creation
 func (m *UserService) CreateUser(ctx context.Context, req service.CreateUserRequest) (*models.UserResponse, error) {
 	args := m.Called(ctx, req)
 	if args.Get(0) == nil {
@@ -20,21 +23,25 @@ func (m *UserService) CreateUser(ctx context.Context, req service.CreateUserRequ
 	return args.Get(0).(*models.UserResponse), args.Error(1)
 }
 
+// UpdateUser mocks updating the user
 func (m *UserService) UpdateUser(ctx context.Context, id string, req service.UpdateUserRequest) (*models.UserResponse, error) {
 	args := m.Called(ctx, id, req)
 	return args.Get(0).(*models.UserResponse), args.Error(1)
 }
 
+// DeleteUser mocks deletion of user
 func (m *UserService) DeleteUser(ctx context.Context, id string) error {
 	args := m.Called(ctx, id)
 	return args.Error(0)
 }
 
+// GetUser mocks retrieving users with filters and pagination
 func (m *UserService) GetUsers(ctx context.Context, filter service.UserFilter, pagination service.Pagination) ([]models.UserResponse, error) {
 	args := m.Called(ctx, filter, pagination)
 	return args.Get(0).([]models.UserResponse), args.Error(1)
 }
 
+// GetUserByID gets specific user with the id specified
 func (m *UserService) GetUserByID(ctx context.Context, id string) (*models.UserResponse, error) {
 	args := m.Called(ctx, id)
 	return args.Get(0).(*models.UserResponse), args.Error(1)
